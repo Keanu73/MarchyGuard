ebe0580e93e6556beb65c5a07800422df1403393
Keanu73 <guestone63@gmail.com>
2020-11-19 22:20:25 +0000

    Add Moderator exemption (in theory)

--- src/modules/VoiceChatModule.ts
+++ src/modules/VoiceChatModule.ts
@@ -6,8 +6,11 @@ export class VoiceChatModule {
   @On("voiceStateUpdate")
   async onJoiningVoice([oldState, newState]: VoiceState[], _client: Client): Promise<void> {
     // If they are already in AFK channel, forget about it
-    const voiceChatChannel = newState.guild.channels.cache.find((channel) => channel.name === "voice-chat");
+    // Check if mod
+    // If so return
     const member = newState.member as GuildMember;
+    if (member.roles.cache.find((role) => role.name === "Moderator")) return;
+    const voiceChatChannel = newState.guild.channels.cache.find((channel) => channel.name === "voice-chat");
     if (!oldState.channel && newState.channel && newState.channelID !== config.afkChannel) {
       const voiceChannel = newState.channel;
       void (voiceChatChannel as TextChannel).updateOverwrite(
