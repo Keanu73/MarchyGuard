a618ca728cc88c3f6856b00205080e5494934d56
Keanu73 <guestone63@gmail.com>
2020-11-19 22:12:30 +0000

    Change docker version to supported one and fix VoiceChatModule to support new channel changes

--- src/modules/VoiceChatModule.ts
+++ src/modules/VoiceChatModule.ts
@@ -4,9 +4,9 @@ import { config } from "../Config";
 
 export class VoiceChatModule {
   @On("voiceStateUpdate")
-  async onJoiningVoice([oldState, newState]: VoiceState[], client: Client): Promise<void> {
+  async onJoiningVoice([oldState, newState]: VoiceState[], _client: Client): Promise<void> {
     // If they are already in AFK channel, forget about it
-    const voiceChatChannel = await client.channels.fetch(config.voiceChatChannel, false, true);
+    const voiceChatChannel = newState.guild.channels.cache.find((channel) => channel.name === "voice-chat");
     const member = newState.member as GuildMember;
     if (!oldState.channel && newState.channel && newState.channelID !== config.afkChannel) {
       const voiceChannel = newState.channel;
