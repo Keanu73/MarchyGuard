b6641471664d6553d76004947eb6e798dd44e1c4
Keanu73 <guestone63@gmail.com>
2020-11-28 21:57:28 +0000

    Bumps minor version - deploys optimisation to VoiceChatModule permission overwrites

--- src/modules/VoiceChatModule.ts
+++ src/modules/VoiceChatModule.ts
@@ -11,7 +11,6 @@ export class VoiceChatModule {
     const afkChannel = newState.guild.channels.cache.find((channel) => channel.name === "AFK");
     const voiceChatChannel = newState.guild.channels.cache.find((channel) => channel.name === "voice-chat");
     // If they just joined a voice channel that isn't the AFK channel..
-    // TODO: Investigate removing permission overwrites so we can keep the permissions as clean as possible
     if (!oldState.channel && newState.channel && newState.channel !== afkChannel) {
       // Let them see the voice-chat channel, logging which voice channel they joined.
       const voiceChannel = newState.channel;
@@ -24,27 +23,29 @@ export class VoiceChatModule {
     } else if (oldState.channel && !newState.channel) {
       // Remove their permissions to see the voice channel
       const voiceChannel = oldState.channel;
-      void (voiceChatChannel as TextChannel).updateOverwrite(
-        member,
-        { VIEW_CHANNEL: false },
-        `${member.user.username} joined ${voiceChannel.name}`,
-      );
+      void (voiceChatChannel as TextChannel).permissionOverwrites
+        .get(member.id)
+        ?.delete(`${member.user.username} left ${voiceChannel.name}`);
       // If they move from the AFK channel to a voice channel..
     } else if (oldState.channel && newState.channel && newState.channel !== afkChannel) {
-      const voiceChannel = oldState.channel;
+      const voiceChannel = newState.channel;
       void (voiceChatChannel as TextChannel).updateOverwrite(
         member,
         { VIEW_CHANNEL: true },
         `${member.user.username} joined ${voiceChannel.name}`,
       );
       // However, if they move to the AFK channel..
-    } else if (oldState.channel && newState.channel && oldState.channel !== afkChannel) {
+    } else if (
+      oldState.channel &&
+      newState.channel &&
+      oldState.channel !== afkChannel &&
+      newState.channel === afkChannel
+    ) {
+      // Remove their permissions so you can't just spy on #voice-chat
       const voiceChannel = oldState.channel;
-      void (voiceChatChannel as TextChannel).updateOverwrite(
-        member,
-        { VIEW_CHANNEL: false },
-        `${member.user.username} joined ${voiceChannel.name}`,
-      );
+      void (voiceChatChannel as TextChannel).permissionOverwrites
+        .get(member.id)
+        ?.delete(`${member.user.username} left ${voiceChannel.name}`);
     }
   }
 }
