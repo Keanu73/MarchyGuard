c21db938195ad491aec595d40ed16b7504dea93c
Keanu73 <guestone63@gmail.com>
2021-01-20 21:47:22 +0000

    Closes #12 by creating further type safety fixes

--- src/modules/AgreementModule.ts
+++ src/modules/AgreementModule.ts
@@ -9,25 +9,41 @@ export abstract class AgreementModule {
     if (user.bot) return;
     // Store guild, member and the role/channel so we can fetch them later.
     const guild = reaction.message.guild;
-    const member = guild?.members.resolve(user);
-    const newMemberRole = guild?.roles.cache.find((r) => r.name === "Follower");
-    const agreementChannel = guild?.channels.cache.find(
-      (channel) => channel.name === config.agreementChannel ?? "lobby",
-    ) as TextChannel;
+    if (guild && guild.id === process.env.DISCORD_GUILD_ID) {
+      const member = guild.members.resolve(user);
+      const newMemberRole = guild.roles.cache.find((r) => r.name === "Follower");
+      const agreementChannel = guild.channels.cache.find(
+        (channel) => channel.name === config.agreementChannel ?? "lobby",
+      ) as TextChannel;
 
-    // Check if message is the agreement message
-    if (reaction.message.channel === agreementChannel && newMemberRole) {
-      // Check if they have the Follower role already
-      const memberHasRole = member?.roles.cache.array().includes(newMemberRole);
-      if (!memberHasRole) {
-        // If they don't have it, give them it.
-        void member?.roles.add(newMemberRole, "Successfully verified");
-        void reaction.users.remove(user);
-        console.info(
-          `[${new Date().getHours()}:${String(new Date().getMinutes()).padStart(2, "0")}] Added ${user.username} to the server`,
+      // Check if message is the agreement message
+      if (member && reaction.message.channel === agreementChannel && newMemberRole) {
+        // Check if they have the Follower role already
+        const memberHasRole = member.roles.cache.has(newMemberRole.id);
+        if (!memberHasRole) {
+          // If they don't have it, give them it.
+          void member.roles.add(newMemberRole, "Successfully verified");
+          void reaction.users.remove(user);
+          console.info(
+            `[${new Date().getHours()}:${String(new Date().getMinutes()).padStart(2, "0")}] Added ${user.username} to the server`,
+          );
+        }
+        // Otherwise, don't do anything.
+      } else {
+        console.error(
+          `[${new Date().getHours()}:${String(new Date().getMinutes()).padStart(
+            2,
+            "0",
+          )}] The bot's AgreementModule is being a GIANT PEPEGA`,
         );
       }
-      // Otherwise, don't do anything.
+    } else {
+      console.error(
+        `[${new Date().getHours()}:${String(new Date().getMinutes()).padStart(
+          2,
+          "0",
+        )}] The bot's AgreementModule is being an EXTRA GIANT PEPEGA AND I HAVE NO IDEA WHY`,
+      );
     }
   }
 }
